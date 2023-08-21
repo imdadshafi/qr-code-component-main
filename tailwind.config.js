fcaffe5dc7afc3fa4cce8daff8c35cd56101c41a
imdad shafi <imdadshafi@gmail.com>
2023-08-22 02:58:18 +0500

    setup project and convert the design into html

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'white': 'hsl(0, 0%, 100%)',
+        'blue-200' : 'hsl(212, 45%, 89%)',
+
+      },
+    },
+  },
+  plugins: [],
+}
+
